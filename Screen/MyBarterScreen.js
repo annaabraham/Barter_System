f37f12512c029f8918a0d3eb491dacbc7b7fc0e0
annaabraham <60785174+annaabraham@users.noreply.github.com>
2021-09-12 11:15:30 +0530

    Add files via upload

---
+++ Screen/MyBarterScreen.js
@@ -0,0 +1,8 @@
+import React,{component} from 'react';
+import{Text,View,StyleSheet,KeyboardAvoidingView,Alert,TextInput} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+
+export default class MyBarter extends component{
+    
+}
